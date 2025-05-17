080c33c55b44352fe84c3d71c53ddc61eb41c500
Sorin Gore <soringoredev@gmail.com>
2025-05-17 12:19:07 +0300

    Changes

---
+++ src/TemaCereri.java
@@ -0,0 +1,30 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class TemaCereri {
+    public static void main(String[] args) {
+        try {
+            //Creaza clientul HTTP
+            HttpClient client = HttpClient.newHttpClient();
+
+            //Creaza request-ul GET
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("https://jsonplaceholder.typicode.com/posts"))
+                    .GET()
+                    .build();
+
+            //Trimite request-ul si primeste raspunsul
+            HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
+
+            //Afisam raspunsul
+            System.out.println("Status: " + response.statusCode());
+            System.out.println("Body: " + response.body());
+        }
+
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
